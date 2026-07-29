1f6b7a2392b6dcb91aac847223fafa522f86c831
KrinaPatel24 <161822814+KrinaPatel24@users.noreply.github.com>
2026-07-29 11:02:39 +0530

    UTF BOM error solved

--- src/main/java/com/example/Government/subsidy/Project/Security/SecurityConfig.java
+++ src/main/java/com/example/Government/subsidy/Project/Security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                                 "/otp/**"
                         ).permitAll()
                         .requestMatchers("/superadmin/**")
-                        .hasRole("SUPER_ADMIN")
+                        .hasRole("ADMIN")
 
                         .anyRequest().authenticated()
                 )
